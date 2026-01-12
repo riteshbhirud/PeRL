aeb1f5986c96f8b4e75340573adb0c23cc9b42c5
MikaStars39 <meloxdmy@qq.com>
2026-01-12 23:37:08 +0800

    [rebuild] inhouse

--- scripts/data/prepare_verl_aime_2024.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euox pipefail
-
-python3 data/aime2024.py\
-    --local_dataset_path "/mnt/llm-train/users/explore-train/qingyu/.cache/aime_2024" \
-    --local_dir "data/aime_2024-verl"
